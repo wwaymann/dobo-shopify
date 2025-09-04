957b8b392644598bc58b9411ee83738db4bd4a23
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-09-03 22:47:07 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import useIframeAutosize from '../hooks/useIframeAutosize';
 
 function MyApp({ Component, pageProps }) {
+  useIframeAutosize();
   return <Component {...pageProps} />;
 }
 
