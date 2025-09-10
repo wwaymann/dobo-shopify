8bd1affcf86cfadc83f7a7a9841dea430ad8474c
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-09-09 21:09:36 -0300

    Update [handle].js

--- pages/product/[handle].js
+++ pages/product/[handle].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { loadSessionDesign, saveSessionDesign } from '@/lib/designStore';
+import { loadSessionDesign, saveSessionDesign } from '../../lib/designStore';
 
 useEffect(() => {
   let cancelled = false;
