6f3e441630f18e3594d331dbb7c6622ed563d865
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-09-17 22:16:18 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1034,6 +1034,7 @@ designMetaRef.current = payload?.meta || payload?.doboMeta || snapshot?.meta || 
               className="d-flex justify-content-center align-items-end"
               style={{
                 height: "100%",
+                "--zoom": 0.75,
                 transform: "scale(var(--zoom))",
                 transformOrigin: "50% 70%",
                 willChange: "transform",
