99d9e0675e3ea1456e581da945a2d97aa65eb784
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-10-14 17:30:42 -0300

    Update HomePage.jsx

--- features/home/HomePage.jsx
+++ features/home/HomePage.jsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 import styles from "../../styles/home.module.css";
 import dynamic from "next/dynamic";
 
-import { cartCreateAndRedirect } from "../lib/shopifyStorefront";
+
 
 import { cartCreateAndRedirect, toGid } from "../../lib/checkout";
 
