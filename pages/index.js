c9666bb490b6942fae4a57d08475c4b834ce2aec
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-10-14 21:37:48 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,1 @@
-// pages/index.js
-import React from "react";
-import Head from "next/head";
-import dynamic from "next/dynamic";
-
-const HomePage = dynamic(() => import("../features/home/HomePage"), { ssr: false });
-
-export default function IndexPage() {
-  return (
-    <>
-      <Head>
-        <title>DOBO</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <HomePage />
-    </>
-  );
-}
+export { default } from "../features/home/HomePage";
