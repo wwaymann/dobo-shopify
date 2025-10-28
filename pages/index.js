b48320589b8d5fe635f29a45d033443b964a4e75
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-10-28 11:22:29 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/home.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import dynamic from "next/dynamic";
 import { exportPreviewDataURL, dataURLtoBase64Attachment, loadLocalDesign } from '../lib/designStore';
-import HomePage from "../HomePage";
+import HomePage from "../features/home/HomePage";
 
 // al inicio del archivo, junto a otros useRef/useState
 const initFromURLRef = { current: false };
