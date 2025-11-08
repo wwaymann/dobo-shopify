36120ac14c075ce6baf198b5ff2b6e1eaf557230
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-08 19:29:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1921,7 +1921,7 @@ designMetaRef.current = payload?.meta || payload?.doboMeta || snapshot?.meta || 
     style={{
       zIndex: 3,
       position: "absolute",
-      bottom: "35vh",
+      bottom: "40vh",
       left: "50%",
       transform: "translateX(-50%)",
       pointerEvents: "auto",
