bc52224cb8769be94e8f861e87b9d27c5fd36771
Walter Waymann <59487134+wwaymann@users.noreply.github.com>
2025-11-21 18:32:24 -0300

    Update index.js

--- pages/pruebas/texto-maceta/index.js
+++ pages/pruebas/texto-maceta/index.js
@@ -2,17 +2,8 @@ import MacetaTest from "../../../components/pruebas/MacetaTest";
 
 export default function TestMacetaPage() {
   return (
-    <div
-      style={{
-        width: "100%",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "20px"
-      }}
-    >
-      <h2>Prueba DOBO – Texto Adaptado a Macetas</h2>
+    <div style={{ width: "100%", minHeight: "100vh", padding: "20px" }}>
+      <h2>Prueba DOBO – Texto Adaptado</h2>
       <MacetaTest />
     </div>
   );
